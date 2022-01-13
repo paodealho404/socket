3b93fd361069794c491eb842569677ed0bf8628c
Lucas <lucaslcf2001@hotmail.com>
2022-01-13 17:26:47 -0300

    Some changes
    Ta quase

--- client/client.py
+++ client/client.py
@@ -1,12 +1,44 @@
 import socket
+import os
+import platform 
 
 # HOST = '127.0.0.1'
 HOST = socket.gethostbyname(socket.gethostname())
 PORT = 6656
 ADDR = (HOST, PORT)
-SIZE = 1024
+SIZE = 4096
 FORMAT = "utf-8" 
 DISCONNECT_MSG = "!DISCONNECT"
+
+def list_files(caminho):
+    # files = os.listdir(caminho)
+    files = os.scandir(caminho)
+    # print(files)
+    for path in files:
+        if path.is_file():
+            print(f"FILE\t {path.name}")
+        elif path.is_dir():
+            print(f"DIR\t {path.name}")
+
+def get_path(file): 
+    separator = os.sep
+    return f"{os.getcwd()}{separator}{file}"
+
+def send_file(socket, file_path, receiver):
+    try:
+        file = open(file_path, 'rb')
+        file_name = file_path.split(os.sep)[-1]
+    except PermissionError:
+        return "some default data"
+    else:
+        with file:
+            bytes = file.read()
+            
+            socket.send(file_name.encode(FORMAT))
+            socket.send(bytes)
+            # socket.send(f"{file_name}>{bytes}".encode(FORMAT))
+            
+
 
 def main():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -15,16 +47,21 @@ def main():
     
     connected = True
     while connected:
-        print("Faça sua pesquisa no Wikipedia.")
-        msg = input("> ")
+        # list_files('files')
         
-        client.send(msg.encode(FORMAT))
+        file_path = get_path(input("> "))
         
-        if msg == DISCONNECT_MSG:
+        send_file(client, file_path, ' ')
+        # msg = input("> ")
+        
+        
+        # client.send(msg.encode(FORMAT))
+        
+        if file_path == DISCONNECT_MSG:
             connected = False
         else:
-            msg = client.recv(SIZE).decode(FORMAT)
-            print(f"[SERVER] {msg}")
+            file_path = client.recv(SIZE).decode(FORMAT)
+            print(f"[SERVER] {file_path}")
 
 if __name__ == "__main__":
     main()
--- server/server.py
+++ server/server.py
@@ -1,11 +1,12 @@
 import socket
 import threading
-import wikipedia
+import os
+
 # HOST = '127.0.0.1'
 HOST = socket.gethostbyname(socket.gethostname())
 PORT = 6656
 ADDR = (HOST, PORT)
-SIZE = 1024
+SIZE = 4096
 FORMAT = "utf-8" 
 DISCONNECT_MSG = "!DISCONNECT"
 
@@ -14,15 +15,29 @@ def handle_client(conn, addr):
     
     connected = True
     while connected:
-        msg = conn.recv(SIZE).decode(FORMAT)
-        if msg == DISCONNECT_MSG:
-            connected = False
+        file_name = conn.recv(SIZE).decode(FORMAT)
+        print(file_name)
+        data = conn.recv(SIZE)
+        # if msg == DISCONNECT_MSG:
+        #     connected = False
         
-        print(f"[{addr}] {msg}")
-        # msg = f"Msg received: {msg}"
-        msg = wikipedia.summary(msg, sentences=1)
+        # file_name, data = msg.split(">", 1)
+        
+        # print(f"[{addr}] {data}")
+        print(f"[{addr}]")
+        
+        msg = f"File {file_name} received"
+        # data = wikipedia.summary(data, sentences=1)
         conn.send(msg.encode(FORMAT))
         
+        try:
+            os.mkdir("files")
+        except:
+            pass  
+        
+        file = open(f"files/{file_name}", 'wb')
+        file.write(data)
+        file.close()
     conn.close()
 
 def main():
@@ -31,7 +46,6 @@ def main():
     server.bind(ADDR)
     server.listen()
     print(f"[LISTENING] Server is listening on {HOST}: {PORT}")
-    
     while True: 
         conn, addr = server.accept()
         thread = threading.Thread(target=handle_client, args=(conn, addr))
