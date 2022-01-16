a3250599509a294aa16ae6c18931cfcfd3d5adc6
Lucas <lucaslcf2001@hotmail.com>
2022-01-16 12:20:01 -0300

    Add handle_client

--- server/server.py
+++ server/server.py
@@ -11,40 +11,53 @@ FORMAT = "utf-8"
 DISCONNECT_MSG = "!DISCONNECT"
 MSG_SEP = "|"
 
+def handle_upload(socket, header):
+    # print(header)
+    file_size, file_name = header.split(MSG_SEP,1) 
+    file_path = f"{os.getcwd()}{os.sep}files{os.sep}{file_name}"
+    # print(file_name, file_size)
+    
+    try:
+        os.mkdir(f"{os.getcwd()}{os.sep}files")
+    except:
+        pass  
+    
+    to_receive = int(file_size)
+    
+    with open(file_path, 'wb') as file:
+        while True:
+            if to_receive > 0:
+                bytes_read = socket.recv(min(to_receive, SIZE))               
+                file.write(bytes_read)
+                to_receive -= len(bytes_read)
+            else:
+                break
+    
+    
+    msg = f"File {file_name} received and uploaded to the server."
+    
+    socket.send(msg.encode(FORMAT))
+
+def handle_download(socket, file_name):
+    return
 
 def handle_client(conn, addr):
     print(f"[NEW CONNECTION] {ADDR} connected")
     
     connected = True
+    commands = { 
+        "UPLD": handle_upload,
+        "DWLD": handle_download,
+        # "SHOW": handle_show_files,
+        # "DELT": handle_deletion,
+        # "HELP": handle_help,
+        # "DISC": handle_disconnect,
+    }
     while connected:
         cmd, msg = conn.recv(SIZE).decode(FORMAT).split(MSG_SEP,1) 
-        print(cmd, msg)
-        file_size, file_name = msg.split(MSG_SEP,1) 
-        file_path = f"{os.getcwd()}{os.sep}files{os.sep}{file_name}"
-        print(file_name, file_size)
         
-        try:
-            # print(f"{os.getcwd()}{os.sep}files")
-            os.mkdir(f"{os.getcwd()}{os.sep}files")
-        except:
-            pass  
+        commands.get(cmd)(conn, msg)
         
-        to_receive = int(file_size)
-        
-        with open(file_path, 'wb') as file:
-            while True:
-                if to_receive > 0:
-                    bytes_read = conn.recv(min(to_receive, SIZE))               
-                    file.write(bytes_read)
-                    to_receive -= len(bytes_read)
-                else:
-                    break
-        
-        print(f"[{addr}]")
-        
-        msg = f"File {file_name} received"
-        
-        conn.send(msg.encode(FORMAT))
 
     conn.close()
 
